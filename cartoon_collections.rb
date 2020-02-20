84ab8baa7f3e7b41ee5856d3e64a6324994e04be
Tim Donoghue <tmd112593@gmail.com>
2020-02-20 19:55:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(arr)
   arr.each_with_index{ |name, idx| print "#{idx + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)
   # Your code here
 end
 
